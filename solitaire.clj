d5e9f114a22bf0446076facae8f42e68e2ac0f80
Benny Tsai <benny.tsai@gmail.com>
2010-11-20 22:33:16 -0700

    implemented keying decks with passphrases

--- solitaire.clj
+++ solitaire.clj
@@ -18,47 +18,60 @@
 	out-nums (map #(mod (f %1 %2) 26) in-nums keystream)]
     (to-text out-nums)))
 
-(defn move-down [card cards]
-  (if-not (= card (peek cards))
-    (let [card-idx (first (index-filter #{card} cards))]
-      (swap card-idx (inc card-idx) cards))
-    (let [[top-card & middle-cards] (pop cards)]
+(defn move-down [card deck]
+  (if (not= card (peek deck))
+    (let [card-idx (first (index-filter #{card} deck))]
+      (swap card-idx (inc card-idx) deck))
+    (let [[top-card & middle-cards] (pop deck)]
       (vec (concat [top-card] [card] middle-cards)))))
 
-(defn triple-cut [cards]
-  (let [[joker-idx-1 joker-idx-2] (index-filter #{\A \B} cards)
-	top-cards (subvec cards 0 joker-idx-1)
-	middle-cards (subvec cards joker-idx-1 (inc joker-idx-2))
-	bottom-cards (subvec cards (inc joker-idx-2))]
+(defn triple-cut [deck]
+  (let [[joker-idx-1 joker-idx-2] (index-filter #{\A \B} deck)
+	top-cards (subvec deck 0 joker-idx-1)
+	middle-cards (subvec deck joker-idx-1 (inc joker-idx-2))
+	bottom-cards (subvec deck (inc joker-idx-2))]
     (vec (concat bottom-cards middle-cards top-cards))))
 
-(defn count-cut [cards]
-  (let [bottom-card (peek cards)
-	offset (to-number bottom-card)
-	top-cards (subvec cards 0 offset)
-	middle-cards (pop (subvec cards offset))]
-    (vec (concat middle-cards top-cards [bottom-card]))))
+(defn count-cut
+  ([deck]
+     (let [bottom-card (peek deck)]
+       (count-cut (to-number bottom-card) deck)))
+  ([offset deck]
+     (let [bottom-card (peek deck)
+	   top-cards (subvec deck 0 offset)
+	   middle-cards (pop (subvec deck offset))]
+       (vec (concat middle-cards top-cards [bottom-card])))))
 
-(defn solitaire [[_ cards]]
-  (let [shuffled-cards (->> cards
-			    (move-down \A)
-			    (move-down \B)
-			    (move-down \B)
-			    triple-cut
-			    count-cut)
-	top-card (first shuffled-cards)
-	output-card (shuffled-cards (to-number top-card))]
+(defn solitaire [deck]
+  (->> deck
+       (move-down \A)
+       (move-down \B)
+       (move-down \B)
+       triple-cut
+       count-cut))
+
+(defn solitaire-output [[_ deck]]
+  (let [shuffled-deck (solitaire deck)
+	top-card (first shuffled-deck)
+	output-card (get shuffled-deck (to-number top-card))]
     (if (#{\A \B} output-card)
-      (recur [nil shuffled-cards])
-      [(to-number output-card) shuffled-cards])))
+      (recur [nil shuffled-deck])
+      [(to-number output-card) shuffled-deck])))
 
-(defn make-keystream [cards]
-  (map first (drop 1 (iterate solitaire [nil cards]))))
+(defn make-keystream [deck]
+  (let [solitaire-outputs (iterate solitaire-output [nil deck])]
+    (map first (next solitaire-outputs))))
 
-(defn encrypt [plaintext cards]
-  (combine plaintext (make-keystream cards) +))
+(defn encrypt [plaintext deck]
+  (combine plaintext (make-keystream deck) +))
 
-(defn decrypt [ciphertext cards]
-  (combine ciphertext (make-keystream cards) -))
+(defn decrypt [ciphertext deck]
+  (combine ciphertext (make-keystream deck) -))
 
-(def cards (vec (concat (range 1 53) [\A \B])))
+(defn key-deck [deck passphrase]
+  (let [offsets (map inc (to-numbers passphrase))]
+    (reduce (fn [deck offset] (->> deck solitaire (count-cut offset)))
+	    deck
+	    offsets)))
+
+(def deck (vec (concat (range 1 53) [\A \B])))
