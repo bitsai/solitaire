e96cdc68fe4d0c3dface5d8fe596f810ded7cab3
Benny Tsai <benny.tsai@gmail.com>
2010-11-22 15:16:03 -0700

    added commandline encrypting/decrypting

--- solitaire.clj
+++ solitaire.clj
@@ -1,3 +1,12 @@
 (ns solitaire
   (:use algorithm))
 
+(defn print-usage []
+  (println "Usage: clj solitaire.clj [-e|-d] <key phrase> <text>"))
+
+(if-let [[encrypt-or-decrypt key-phrase text] *command-line-args*]
+  (cond
+   (= "-e" encrypt-or-decrypt) (println (encrypt text key-phrase))
+   (= "-d" encrypt-or-decrypt) (println (decrypt text key-phrase))
+   :else (print-usage))
+  (print-usage))
